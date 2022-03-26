6694e5da070cb3100d5d3c709de9f9be41e695d0
Golam Mursalin <golammursalin309@gmail.com>
2022-03-26 11:24:52 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+//Components
+import Header from "./components/Header/Header";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// CSS
+import "./Header.css";
+const Header = () => {
+  return (
+    <nav className="header">
+      <h3>Better Buys</h3>
+    </nav>
+  );
+};
+
+export default Header;
